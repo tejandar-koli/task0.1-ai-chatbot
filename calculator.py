5ffef36849053d3df7b4b454ef3ea3019266e804
tejandar-koli <tejandarkoli1@gmail.com>
2026-04-16 17:09:41 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,30 @@
+num1 = float(input("Enter a number: "))
+op = input("Enter operation (+, -, *, /,//, %, ^): ")
+num2 = float(input("Enter a number: "))
 
+if op == "+":
+    print(num1 + num2)
+
+elif op == "-":
+    print(num1 - num2)
+
+elif op == "*":
+    print(num1 * num2)
+
+elif op == "/":
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Cannot divide by zero")
+
+elif op == "%":
+    if num2 != 0:
+        print(num1 % num2)
+    else:
+        print("Cannot divide by zero")
+
+elif op == "^":
+    print(num1 ** num2)
+
+else:
+    print("Invalid operation")
